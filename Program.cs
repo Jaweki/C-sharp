292928bc778539c6deb9fca2e59a0e0dc14b5bcc
Jack Weru Kioni <werukioni@gmail.com>
2023-01-31 16:23:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Defining{
+    class Program {
+        static void Main(string[] args){
+            Book b1 = new Book("War and Peace", "Leo", 825);
+            Book b2 = new Book("New arrival", "Tom", 439);
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
+        }
+        }
+}
