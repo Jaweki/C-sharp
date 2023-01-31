67f8023fd64fe5f6a51bdd36e3d4de5eebf00dfb
Jack Weru Kioni <werukioni@gmail.com>
2023-01-31 15:53:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace While{
+    class Program{
+        static void Main(string[] args){
+            string inputStr = " ";
+            Console.WriteLine("Basic while() loop: ");
+            while(inputStr != "exit"){
+                inputStr = Console.ReadLine();
+                Console.WriteLine("You entered: {0}", inputStr);
+            }
+        }
+    }
+}
