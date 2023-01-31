0445f3320fce449c0cb97d4c5c02cb5213606725
Jack Weru Kioni <werukioni@gmail.com>
2023-01-31 16:04:09 +0300

    Add files via upload

---
+++ 1. Named and default parameters/Program.cs
@@ -0,0 +1,6 @@
+﻿//NAMED AND DEFAULT PARAMETERS
+void PrintWithPrefix(string theStr, string Prefix = " "){
+    Console.WriteLine($"{Prefix}{theStr}");
+}
+
+PrintWithPrefix(Prefix : "%", theStr : "Hello There");
