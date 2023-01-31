a97d655d99ac8e72abc6a782e9d81b1e969f4cac
Jack Weru Kioni <werukioni@gmail.com>
2023-01-31 15:56:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string inputStr = " ";
+Console.WriteLine("Do while () loop");
+do{
+    inputStr = Console.ReadLine();
+    if (inputStr == "exit")
+    {
+        break;
+    }
+    Console.WriteLine("You entered: {0}",inputStr);
+}while (inputStr != "exit");
