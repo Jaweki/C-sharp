37cbb7f3cc035e82473ad71c5484e2d52b3f82a9
Jack Weru Kioni <werukioni@gmail.com>
2023-01-31 15:18:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace IfStatement{
+    class Program{
+        static void Main(string[]args){
+            int theVal = 50;
+            if (theVal == 50){
+                Console.WriteLine("The val is 50");
+            }
+            else if (theVal>50 || theVal<=60){
+                Console.WriteLine("The val is between 50 and 60");
+            }
+            else {
+                Console.WriteLine("The val is someting else");
+            }
+        }
+    }
+}
