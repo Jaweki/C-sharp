a7b079648f1e6990e9332a74a7c9c4aae5917a7b
Jack Weru Kioni <werukioni@gmail.com>
2023-01-31 16:13:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿float milesToKm(float miles){
+    float result = miles*1.6f;
+    return result;
+}
+
+void PrintWithPrefix(string theStr){
+    Console.WriteLine($"::>{theStr}");
+}
+//Console.WriteLine($"The result is {milesToKm(52.0f)}");
+PrintWithPrefix("Test string");
